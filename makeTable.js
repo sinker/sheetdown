39cbd394a5f9224a8b7566875fbed2844ed6080a
sinker <dansinker@gmail.com>
2014-05-20 09:34:04 -0500

    updated the reference URL to spreadsheets

--- makeTable.js
+++ makeTable.js
@@ -3,14 +3,14 @@ var csv = require('binary-csv')
 var concat = require('concat-stream')
 
 module.exports = function makeTable(KEY, callback) {
-  var base = 'https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=' 
-  var query = '&single=true&gid=0&output=csv'
+  var base = 'https://docs.google.com/spreadsheets/d/'
+  var query = '/export?gid=0&amp;format=csv'
   var URL = base + KEY + query
-    
+
   var csvParser = csv({json: true})
-  
+
   request(URL).pipe(csvParser).pipe(concat(rows))
-  
+
   function rows(data) {
     var table = '|'
     var headers = Object.keys(data[0])
@@ -19,7 +19,7 @@ module.exports = function makeTable(KEY, callback) {
       table += key + '|'
       underHeaders += ' ------ |'
     })
-    
+
     table += '\n|' + underHeaders + '\n'
     data.map(function(row) {
       var values = headers.map(function(h) { return row[h] })
@@ -28,4 +28,3 @@ module.exports = function makeTable(KEY, callback) {
     return callback("null", table)
   }
 }
-
